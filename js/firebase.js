18f37fc8be7027f92cad3bd86afcfc7689004eae
edulanzarin <edulanzarin@outlook.com>
2024-08-22 14:05:29 -0300

    Erro no cadastro

--- js/firebase.js
+++ js/firebase.js
@@ -1,35 +1,3 @@
-function _0x56cb(_0x5109c4, _0x2f93c8) {
-  const _0x1ae049 = _0x1ae0();
-  return (
-    (_0x56cb = function (_0x56cb6b, _0x21a2b7) {
-      _0x56cb6b = _0x56cb6b - 0xe0;
-      let _0x5ab308 = _0x1ae049[_0x56cb6b];
-      return _0x5ab308;
-    }),
-    _0x56cb(_0x5109c4, _0x2f93c8)
-  );
-}
-const _0x2fe133 = _0x56cb;
-(function (_0x28f7e7, _0x47621e) {
-  const _0xa788dd = _0x56cb,
-    _0x4d8652 = _0x28f7e7();
-  while (!![]) {
-    try {
-      const _0x5c93b8 =
-        parseInt(_0xa788dd(0xe9)) / 0x1 +
-        (parseInt(_0xa788dd(0xe3)) / 0x2) * (-parseInt(_0xa788dd(0xe1)) / 0x3) +
-        -parseInt(_0xa788dd(0xe6)) / 0x4 +
-        parseInt(_0xa788dd(0xe8)) / 0x5 +
-        parseInt(_0xa788dd(0xeb)) / 0x6 +
-        parseInt(_0xa788dd(0xe0)) / 0x7 +
-        -parseInt(_0xa788dd(0xee)) / 0x8;
-      if (_0x5c93b8 === _0x47621e) break;
-      else _0x4d8652["push"](_0x4d8652["shift"]());
-    } catch (_0x12e511) {
-      _0x4d8652["push"](_0x4d8652["shift"]());
-    }
-  }
-})(_0x1ae0, 0x84e25);
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import {
   getAuth,
@@ -38,42 +6,22 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js";
+
 const firebaseConfig = {
-    apiKey: _0x2fe133(0xe7),
-    authDomain: _0x2fe133(0xea),
-    projectId: _0x2fe133(0xe5),
-    storageBucket: _0x2fe133(0xed),
-    messagingSenderId: _0x2fe133(0xec),
-    appId: _0x2fe133(0xe4),
-    measurementId: _0x2fe133(0xe2),
-  },
-  app = initializeApp(firebaseConfig),
-  auth = getAuth(app),
-  database = getDatabase(app),
-  storage = getStorage(app);
-function _0x1ae0() {
-  const _0x3bb942 = [
-    "9yssVIc",
-    "G-5RZM4HERL8",
-    "460738zFKyyi",
-    "1:223565529814:web:d992bd60cba7aadb54ff77",
-    "quanttum-site",
-    "12436AviURi",
-    "AIzaSyDWgjOPqQZr4A8YlnwOQpr3n_JWncLZOmk",
-    "1233200ErgaPu",
-    "1034695IsiuPz",
-    "quanttum-site.firebaseapp.com",
-    "230172OOEHCd",
-    "223565529814",
-    "quanttum-site.appspot.com",
-    "7386688BmHDrH",
-    "5895036rIkfvJ",
-  ];
-  _0x1ae0 = function () {
-    return _0x3bb942;
-  };
-  return _0x1ae0();
-}
+  apiKey: "AIzaSyDWgjOPqQZr4A8YlnwOQpr3n_JWncLZOmk",
+  authDomain: "quanttum-site.firebaseapp.com",
+  projectId: "quanttum-site",
+  storageBucket: "quanttum-site.appspot.com",
+  messagingSenderId: "223565529814",
+  appId: "1:223565529814:web:d992bd60cba7aadb54ff77",
+  measurementId: "G-5RZM4HERL8",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+const storage = getStorage(app);
+
 export {
   auth,
   database,
--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,14 @@
-import { auth, createUserWithEmailAndPassword, sendEmailVerification } from './firebase.js';
-import { getDatabase, ref, set, get } from 'https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js';
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+} from "./firebase.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
 
 const slidePage = document.querySelector(".slide-page");
 const nextBtnFirst = document.querySelector(".firstNext");
@@ -18,19 +27,19 @@ let current = 1;
 function showError(step, message) {
   const label = document.querySelector(`.field-${step} .label-${step}`);
   label.textContent = message;
-  label.style.visibility = 'visible';
+  label.style.visibility = "visible";
 }
 
 function hideError(step) {
   const label = document.querySelector(`.field-${step} .label-${step}`);
-  label.style.visibility = 'hidden';
+  label.style.visibility = "hidden";
 }
 
 function checkFields(step) {
   const fields = document.querySelectorAll(`.page:nth-of-type(${step}) input`);
   let valid = true;
 
-  fields.forEach(field => {
+  fields.forEach((field) => {
     if (field.value.trim() === "") {
       showError(step, "Por favor, preencha todos os campos.");
       valid = false;
@@ -41,7 +50,7 @@ function checkFields(step) {
   return valid;
 }
 
-nextBtnFirst.addEventListener("click", function(event) {
+nextBtnFirst.addEventListener("click", function (event) {
   event.preventDefault();
   if (checkFields(1)) {
     slidePage.style.marginLeft = "-25%";
@@ -52,7 +61,7 @@ nextBtnFirst.addEventListener("click", function(event) {
   }
 });
 
-nextBtnSec.addEventListener("click", function(event) {
+nextBtnSec.addEventListener("click", function (event) {
   event.preventDefault();
   if (checkFields(2)) {
     slidePage.style.marginLeft = "-50%";
@@ -63,7 +72,7 @@ nextBtnSec.addEventListener("click", function(event) {
   }
 });
 
-nextBtnThird.addEventListener("click", function(event) {
+nextBtnThird.addEventListener("click", function (event) {
   event.preventDefault();
   if (checkFields(3)) {
     slidePage.style.marginLeft = "-75%";
@@ -74,18 +83,32 @@ nextBtnThird.addEventListener("click", function(event) {
   }
 });
 
-submitBtn.addEventListener("click", async function(event) {
+submitBtn.addEventListener("click", async function (event) {
   event.preventDefault();
 
-  const inputs = document.querySelectorAll(".page:nth-of-type(1) input[type='text']");
-  const nameInput = inputs[0].value.trim(); 
+  const inputs = document.querySelectorAll(
+    ".page:nth-of-type(1) input[type='text']"
+  );
+  const nameInput = inputs[0].value.trim();
   const surnameInput = inputs[1].value.trim();
-  const emailInput = document.querySelector(".page:nth-of-type(2) input[type='email']").value.trim();
-  const passwordInput = document.querySelector(".page:nth-of-type(4) input[type='password']").value.trim();
-  const cargoSelect = document.querySelector(".page:nth-of-type(2) select").value;
-  const setorSelect = document.querySelector(".page:nth-of-type(3) select").value;
-  const segmentoSelect = document.querySelector(".page:nth-of-type(3) select").value;
-  const usuarioInput = document.querySelector(".page:nth-of-type(4) input[type='text']").value.trim();
+  const emailInput = document
+    .querySelector(".page:nth-of-type(2) input[type='email']")
+    .value.trim();
+  const passwordInput = document
+    .querySelector(".page:nth-of-type(4) input[type='password']")
+    .value.trim();
+  const cargoSelect = document.querySelector(
+    ".page:nth-of-type(2) select"
+  ).value;
+  const setorSelect = document.querySelector(
+    ".page:nth-of-type(3) select"
+  ).value;
+  const segmentoSelect = document.querySelector(
+    ".page:nth-of-type(3) select"
+  ).value;
+  const usuarioInput = document
+    .querySelector(".page:nth-of-type(4) input[type='text']")
+    .value.trim();
 
   if (checkFields(4)) {
     try {
@@ -98,12 +121,12 @@ submitBtn.addEventListener("click", async function(event) {
       }
 
       const db = getDatabase();
-      const usuariosRef = ref(db, 'usuarios/');
+      const usuariosRef = ref(db, "usuarios/");
       const snapshot = await get(usuariosRef);
       let emailExists = false;
       let usernameExists = false;
 
-      snapshot.forEach(childSnapshot => {
+      snapshot.forEach((childSnapshot) => {
         const childData = childSnapshot.val();
         if (childData.email === emailInput) {
           emailExists = true;
@@ -122,13 +145,17 @@ submitBtn.addEventListener("click", async function(event) {
       }
 
       // Criar novo usuário se não existir
-      const userCredential = await createUserWithEmailAndPassword(auth, emailInput, passwordInput);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        emailInput,
+        passwordInput
+      );
       const user = userCredential.user;
 
       await sendEmailVerification(user);
 
       // Salvar informações do usuário no banco de dados
-      const userRefNew = ref(db, 'usuarios/' + user.uid);
+      const userRefNew = ref(db, "usuarios/" + user.uid);
       await set(userRefNew, {
         nome: nameInput,
         sobrenome: surnameInput,
@@ -137,32 +164,33 @@ submitBtn.addEventListener("click", async function(event) {
         setor: setorSelect,
         segmento: segmentoSelect,
         usuario: usuarioInput,
-        senha: passwordInput
+        senha: passwordInput,
       });
 
       bullet[current - 1].classList.add("active");
       progressCheck[current - 1].classList.add("active");
       progressText[current - 1].classList.add("active");
 
-      const successLabel = document.querySelector('.field-4 .label-4');
-      successLabel.textContent = 'Cadastro realizado com sucesso. Verifique seu e-mail para confirmar sua conta.';
-      successLabel.style.color = 'rgb(76, 175, 80)'; 
-      successLabel.style.visibility = 'visible';
+      const successLabel = document.querySelector(".field-4 .label-4");
+      successLabel.textContent =
+        "Cadastro realizado com sucesso. Verifique seu e-mail para confirmar sua conta.";
+      successLabel.style.color = "rgb(76, 175, 80)";
+      successLabel.style.visibility = "visible";
 
-      setTimeout(function() {
+      setTimeout(function () {
         location.reload();
       }, 1500);
     } catch (error) {
       console.error(`Erro ao criar usuário: ${error.code} - ${error.message}`);
-      const errorLabel = document.querySelector('.field-4 .label-4');
+      const errorLabel = document.querySelector(".field-4 .label-4");
       errorLabel.textContent = `Erro: ${error.message}`;
-      errorLabel.style.color = 'rgb(236, 118, 118)'; 
-      errorLabel.style.visibility = 'visible';
+      errorLabel.style.color = "rgb(236, 118, 118)";
+      errorLabel.style.visibility = "visible";
     }
   }
 });
 
-prevBtnSec.addEventListener("click", function(event) {
+prevBtnSec.addEventListener("click", function (event) {
   event.preventDefault();
   slidePage.style.marginLeft = "0%";
   bullet[current - 2].classList.remove("active");
@@ -171,7 +199,7 @@ prevBtnSec.addEventListener("click", function(event) {
   current -= 1;
 });
 
-prevBtnThird.addEventListener("click", function(event) {
+prevBtnThird.addEventListener("click", function (event) {
   event.preventDefault();
   slidePage.style.marginLeft = "-25%";
   bullet[current - 2].classList.remove("active");
@@ -180,7 +208,7 @@ prevBtnThird.addEventListener("click", function(event) {
   current -= 1;
 });
 
-prevBtnFourth.addEventListener("click", function(event) {
+prevBtnFourth.addEventListener("click", function (event) {
   event.preventDefault();
   slidePage.style.marginLeft = "-50%";
   bullet[current - 2].classList.remove("active");
